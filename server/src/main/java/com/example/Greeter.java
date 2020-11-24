1dd044e4ae80b54f2f03a8931ad45d65c87b477a
Darkspace21 <40853958+Darkspace21@users.noreply.github.com>
2020-11-24 17:33:01 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,22 +1,22 @@
 package com.example;
 
 /**
-* petite class des familles
+* petite classe des familles.
 */
 
 public class Greeter {
 
   /**
-  * this is a class
-  */
+   * this is a classe.
+   */
   public Greeter() {
 
   }
 
   /**
-  * @param someone name of a person
-  * @return greeting string
-  */
+   * @param someone name of a person
+   * @return greeting string
+   */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
