4437554fd7a447486b4b1b8d428ec5e30a165019
Darkspace21 <40853958+Darkspace21@users.noreply.github.com>
2020-11-25 16:08:09 +0100

    Update package-info.java

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,24 +1,3 @@
-/** premier commentaire.*/
+/**tout mes exemple.*/
 package com.example;
 
-/**
- * This is a class.
- */
-public class Greeter {
-
-  /**
-   * this is a constructor.
-   */
-  public Greeter() {
-
-  }
-
-  /**
-   * @param someone name of a person
-   * @return greeting string
-   */
-
-  public final String greet(final String someone) {
-    return String.format("Hello babacar, %s!", someone);
-  }
-}
